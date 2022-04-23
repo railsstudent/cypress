117dba01516865d350ad8e08d0414b8395089f35
Connie Leung <cleung334@gmail.com>
2022-04-23 16:24:52 +0800

    feat: alias

--- cypress/integration/03-aliases.spec.js
+++ cypress/integration/03-aliases.spec.js
@@ -3,5 +3,36 @@
 describe('Aliases', () => {
   beforeEach(() => {
     cy.visit('/jetsetter');
+
+    cy.get('[data-test="filter-items"]').as('filter-items')
+    cy.get('[data-test="items-unpacked"]').as('unpackedItems')
+    cy.get('[data-test="items-packed"]').as('packedItems')
   });
+
+  describe('verify only items that match the filter are shown on the page', () => {
+    it('it should filter the item', () => {
+      cy.get('@filter-items').type('Tooth')
+      cy.get('@filter-items').invoke('val').as('filterValue')
+      cy.get('@unpackedItems').find('li label').as('unpackedItemsLabels')
+
+      cy.get('@filterValue').then((value) => {
+        cy.get('@unpackedItemsLabels').each(item => {
+          const text = item.text()
+          expect(text).to.contain(value)
+        })
+      })
+    })
+
+    it('should move the item to packed item', () => {
+      cy.get('@filter-items').type('Tooth')
+      cy.get('@unpackedItems').find('li').first().as('firstUnpackedItem')        
+      cy.get('@firstUnpackedItem').find('label').as('firstUnpackedItemLabel')
+      cy.get('@firstUnpackedItemLabel').invoke('text').as('labelText')
+
+      cy.get('@labelText').then(text => { 
+        cy.get('@firstUnpackedItemLabel').click()
+        cy.get('@packedItems').contains(text)
+      })
+    })
+  })
 });
