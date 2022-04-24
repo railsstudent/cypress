719c15fe321e221b94ac467f7d5eb964ea53fc73
Connie Leung <cleung334@gmail.com>
2022-04-24 23:56:53 +0800

    feat: get the current path tests

--- cypress/integration/07-echo-chamber.spec.js
+++ cypress/integration/07-echo-chamber.spec.js
@@ -9,9 +9,14 @@ describe('Initial Page', () => {
     cy.get('[data-test="application-title"]').should('contain', 'Echo Chamber');
   });
 
-  it('should have the title of the application in the window', () => {});
+  it('should have the title of the application in the window', () => {
+    cy.title().should('contains', 'Echo chamber')
+  });
 
-  it('should navigate to "/sign-in" when you click the "Sign In" button', () => {});
+  it('should navigate to "/sign-in" when you click the "Sign In" button', () => {
+    cy.get('[data-test="sign-up"]').click()
+    cy.location('pathname').should('contain.text', '/sign-in')
+  });
 
   it('should navigate to "/sign-up" when you click the "Sign Up" button', () => {});
 });
