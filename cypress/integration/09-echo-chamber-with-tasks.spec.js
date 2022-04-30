7341c246d6706091a3d0a3d5d96c949abafa0a04
Connie Leung <cleung334@gmail.com>
2022-04-30 22:33:52 +0800

    feat: sign up and sign in test

--- cypress/integration/09-echo-chamber-with-tasks.spec.js
+++ cypress/integration/09-echo-chamber-with-tasks.spec.js
@@ -1,11 +1,15 @@
 /// <reference types="cypress" />
 
 const user = {
-  email: `${Date.now()}@example.com`,
+  email: `first@example.com`,
   password: 'password123',
 };
 
 describe('Sign Up', () => {
+  beforeEach(() => {
+    cy.task('reset')
+  })
+
   it('should successfully create a user when entering an email and a password', () => {
     // Sign Up
     cy.visit('/echo-chamber/sign-up');
@@ -21,5 +25,39 @@ describe('Sign Up', () => {
 
     cy.location('pathname').should('contain', '/echo-chamber/posts');
     cy.contains('Signed in as ' + user.email);
+  });
+});
+
+describe('Sign In (Failure case)', () => {
+  beforeEach(() => {
+    cy.task('reset')
+  })
+
+  it('should not sign in with a non-existent user', () => {
+    // Sign In
+    cy.visit('/echo-chamber/sign-in');
+    cy.get('[data-test="sign-in-email"]').type(user.email);
+    cy.get('[data-test="sign-in-password"]').type(user.password);
+    cy.get('[data-test="sign-in-submit"]').click();
+
+    cy.contains('Signed in as ' + user.email).should('not.exist')
+    cy.contains('No such user exists')
+  });
+});
+
+describe('Sign In', () => {
+  beforeEach(() => {
+    cy.task('seed')
+  })
+
+  it('should sign in', () => {
+    // Sign In
+    cy.visit('/echo-chamber/sign-in');
+    cy.get('[data-test="sign-in-email"]').type(user.email);
+    cy.get('[data-test="sign-in-password"]').type(user.password);
+    cy.get('[data-test="sign-in-submit"]').click();
+
+    cy.location('pathname').should('contain', '/echo-chamber/posts')
+    cy.contains('Signed in as ' + user.email)
   });
 });
